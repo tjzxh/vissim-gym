caba51dd1e6c58568953ae32865bc5b6463f5e0b
tjzxh <2990496226@qq.com>
2019-07-21 09:51:22 +0800

    decrease conditional value in large gap

--- vissim_gym/envs/vissim_env.py
+++ vissim_gym/envs/vissim_env.py
@@ -170,7 +170,7 @@ class VissimEnv(Env):
         # red sign for dangerous gap
         if input_info["gap_lead"] < 1 * input_info["vel"]:
             r_t_first = -10
-        if input_info["gap_lead"] > 5 * input_info["vel"] or input_info["gap_lead"] > 80:
+        if input_info["gap_lead"] > 3 * input_info["vel"] or input_info["gap_lead"] > 50:
             norm_dis = input_info["gap_lead"] / self.sensor_dis
             if a_idm > 0:
                 r_t_first = -norm_dis / 2
